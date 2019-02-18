b537bc24a2dc113c83bb992cea5a4698519e6c69
Alexander Bennett <alexben@microsoft.com>
2019-02-18 01:06:54 -0800

    cleaned up code

--- code/dynamicList.tsx
+++ code/dynamicList.tsx
@@ -16,9 +16,7 @@ export const data = Data({
 
 
 export const updateNumOfCards: Override = () => {
-  console.log('OVERRIDE!!!!!!!!!!!!!!!!!!!!!!!')
-  console.log('update on date change')
-  console.log(data.dataObjectfromAPI)
+  // console.log(data.dataObjectfromAPI)
 
   return {
     dataObjectfromAPI: data.dataObjectfromAPI,
@@ -30,7 +28,7 @@ export const dataObjectOfOne: Override = () => {
   return {
       onTap() {
         data.dataObjectfromAPI = ['card 01']
-        console.log(data.dataObjectfromAPI)
+        // console.log(data.dataObjectfromAPI)
       },
   }
 }
@@ -44,7 +42,7 @@ export const dataObjectOfThree: Override = () => {
           'card 02',
           'card 03',
         ]
-        console.log(data.dataObjectfromAPI)
+        // console.log(data.dataObjectfromAPI)
       },
   }
 }
@@ -63,7 +61,7 @@ export const dataObjectOfSeven: Override = () => {
           'card 06',
           'card 07',
         ]
-        console.log(data.dataObjectfromAPI)
+        // console.log(data.dataObjectfromAPI)
       },
   }
 }
@@ -71,43 +69,22 @@ export const dataObjectOfSeven: Override = () => {
 
 
 type Props = { 
-  text: string,
-  // numberOfCards: number,
   dataObjectfromAPI: Array<string>,
-  // height: number,
-  // width: number,
+  height: number,
+  width: number,
 };
 
 export class _dynamicList extends React.Component<Props> {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     dataObjectfromAPIstate: ['foo']
-  //   };
-  // }
-
-
-  // let container;
-  // let containerHeight = this.props.cardHeight * this.state.calEvents.value.length
-  // console.log('re-render scroll container')
 
   cardArray = []
 
-  // Return the component contents in JSX
-  // https://reactjs.org/docs/introducing-jsx.html
   render() {
     console.log('re-render dynamic list')
     this.createCards()
 
-    // this.props.height = 100* this.props.dataObjectfromAPI.length
-    // console.log(this.props.height)
-    // this.props.height = 400
 
-    console.log(this.cardArray)
+    // console.log(this.cardArray)
     return (
-    //   <Frame top={0} height={containerHeight}>
-    //   {this.createAllEventCards()}
-    // </Frame>
 
     <Frame name={'wrapper'} height={100* this.props.dataObjectfromAPI.length} width={300}>
         <Stack width={'100%'} height={'100%'} distribution={'start'} align={'center'} direction={'vertical'} gap={0} padding={0}>
@@ -119,10 +96,6 @@ export class _dynamicList extends React.Component<Props> {
   }
 
   createCards() {
-      console.log('create cards')
-      console.log(this.props.dataObjectfromAPI)
-      console.log('^ props.dataObjectfromAPI')
-
       var starterColor = Color('#55CCFF')
 
 
@@ -131,31 +104,24 @@ export class _dynamicList extends React.Component<Props> {
         const element = this.props.dataObjectfromAPI[index];
         const newColor = Color.darken(starterColor, 6 * index)
 
-        console.log(element)       
         this.cardArray.push(
           <M_card background={newColor} Card_Text={element} height={100} width={300} key={'card'+index}></M_card>
         )        
       }
-      console.log(this.cardArray)
     }
 
 
 
 
-  // Set default values for props if there are none
-  // https://reactjs.org/docs/react-component.html#defaultprops
   static defaultProps: Props = {
-    text: "Hello World!",
-    // numberOfCards: 1,
     dataObjectfromAPI: data.dataObjectfromAPI,
-    // height: (100 * data.dataObjectfromAPI.length),
-    // width: 300,
+    height: (100 * data.dataObjectfromAPI.length),
+    width: 300,
   };
 
   // Add Framer UI for this component (in the properties panel)
   // https://framer.com/learn/docs/components#code
   static propertyControls: PropertyControls<Props> = {
-    text: { type: ControlType.String, title: "Text" },
-    // numberOfCards: { type: ControlType.Number, title: "number of cards" }
+    // text: { type: ControlType.String, title: "Text" },
   };
 }
